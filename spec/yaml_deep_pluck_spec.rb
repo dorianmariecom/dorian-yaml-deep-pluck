5e54d24a8a404a884a5c8f12ea47a962021a1e1e
Dorian Marié <dorian@dorianmarie.com>
2024-08-24 12:03:02 +0200

    add basic integration test using tempfile

--- spec/yaml_deep_pluck_spec.rb
+++ spec/yaml_deep_pluck_spec.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require "tempfile"
 
-RSpec.describe "each" do
+RSpec.describe "yaml-deep-pluck" do
+  it "works" do
+    file = Tempfile.create
+    File.write(file, <<~INPUT)
+      - 1
+      - 2
+      - 3
+    INPUT
+    expect(`cat #{file.path} | bin/yaml-deep-pluck 0`).to eq("1\n")
+  end
 end
